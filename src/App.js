a63a6de2a3dfbd84c63b6ee1f0bf685ba33d4f66
Pham-VN <thatnhitam@gmail.com>
2023-10-31 20:52:57 +0100

    Break-word and input

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './components/form/Form.css';
 // import AddVocabIntoList from './components/AddListVocab.js'
 // import FilterNiveauAndTheme from './components/filter/filter.js';
 import CreateSpan from './test_list_word-react.js';
+// import TestAudio from './components/audio.js';
+
 function App() {
   return (
     <div className="d-flex justify-content-around">
       {/* <AddVocabIntoList/> */}
       <CreateSpan></CreateSpan>
+      {/* <TestAudio/> */}
+     
   </div>)
 }
 
